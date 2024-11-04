4002c60a5e435dc98f62f4bfdbc029eb18bd29b5
Oversized Whisper <184733681+OversizedWhisper@users.noreply.github.com>
2024-11-04 10:39:55 +0000

    New Files

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-Scrollytelling made for the Narrativa Visual Class.
-  The project is based on the story written in the poem "Calçada de Carriche" de Antínio Gedeão
-
-"Calçada de Carriche" is a poem by Portuguese writer António Gedeão that reflects the struggles of the working class and the sense of oppression they face in daily life.
-The poem describes the difficulties of climbing a steep hill, symbolizing the hardships of life and the relentless effort required to overcome them.
-With vivid imagery and rhythmic language, Gedeão captures the resilience and perseverance of people in the face of adversity.
-The poem stands as a powerful metaphor for the social and economic challenges experienced by many.
-
+Lu Scrolltelling
--- main.js
+++ main.js
@@ -14,12 +14,14 @@ window.onload = function () {
 
 // GSAP Code
 function start() {
-  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
 }
+
+// Section one
 
 // Move the GIF from the bottom of the container to the top as you scroll
 gsap.fromTo(
-  ".gif",
+  ".section-one .gif",
   {
     y: "100%", // Inicialmente no fundo da página
   },
@@ -27,7 +29,7 @@ gsap.fromTo(
     y: "1600px", // Movendo a GIF para cima conforme o scroll
     ease: "none",
     scrollTrigger: {
-      trigger: ".gif-container",
+      trigger: ".section-one .gif-container",
       start: "top top",
       end: "bottom bottom",
       scrub: true,
@@ -37,10 +39,10 @@ gsap.fromTo(
 );
 
 // Fading animation for the GIF
-gsap.to(".gif", {
+gsap.to(".section-one .gif", {
   opacity: 1,
   scrollTrigger: {
-    trigger: ".gif",
+    trigger: ".section-one .gif",
     start: "top 100px",
     end: "100px",
     scrub: true,
@@ -52,7 +54,7 @@ gsap.to(".gif", {
 
 // Move the GIF from the bottom of the container to the top as you scroll
 gsap.fromTo(
-  ".horizontal-container",
+  ".section-two .horizontal-container",
   {
     y: "0px", x: "-300vw", // Inicialmente no fundo da página
   },
@@ -72,7 +74,7 @@ gsap.fromTo(
 // Move WALKING
 
 gsap.fromTo(
-  ".walking",
+  ".section-two .walking",
   {
     bottom: "350vh", x: "100vw", // Inicialmente no fundo da página
   },
@@ -89,7 +91,74 @@ gsap.fromTo(
   }
 );
 
-// Rain
+// Section four
+
+// Move the GIF from the bottom of the container to the top as you scroll
+gsap.fromTo(
+  ".section-four .horizontal-container",
+  {
+    y: "0px", x: "-300vw", // Inicialmente no fundo da página
+  },
+  {
+    y: "300vh", x: "0px", // Movendo a GIF para cima conforme o scroll
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".section-four",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true,
+      //markers: true,
+    },
+  }
+);
+
+// Move WALKING
+
+gsap.fromTo(
+  ".section-four .walking",
+  {
+    bottom: "350vh", x: "100vw", // Inicialmente no fundo da página
+  },
+  {
+    bottom: "50vh", x: "0px", // Movendo a GIF para cima conforme o scroll
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".section-four",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true,
+      //markers: true,
+    },
+  }
+);
+
+// Section five
+
+// Define the motion path (you can customize the path points)
+  const path = [
+    { x: 0, y: 0 },
+    { x: 800, y: 1000 },
+    { x: -800, y: 3000 },
+    { x: 0, y: 6000 },
+  ];
+
+  gsap.to(".section-five .gif", {
+    scrollTrigger: {
+      trigger: ".section-five .gif-container",
+      start: "top top",  // when the top of the section reaches the top of the viewport
+      end: "4200px bottom",  // when the bottom of the section reaches the bottom of the viewport
+      scrub: 1,  // smooth animation as you scroll
+      markers: true,
+    },
+    motionPath: {
+      path: "#path",
+      align: '#path',
+      alignOrigin: [0.5, 0.5],
+    },
+    duration: 0.2
+  });
+
+// --------------------------------------- Rain ---------------------------------------
 
 // Function to create a single rain drop
 const createRainDrop = (section) => {
